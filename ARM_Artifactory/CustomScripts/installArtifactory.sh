2b0da26a914ce01ad56fbbe7b43e1b1e7448cc5f
jroquelaure <jonathanr@jfrog.com>
2016-03-18 16:39:18 +0100

    add some trace

--- ARM_Artifactory/CustomScripts/installArtifactory.sh
+++ ARM_Artifactory/CustomScripts/installArtifactory.sh
@@ -2,12 +2,14 @@ $ADMIN_USERNAME=$1
 $ADMIN_PASSWORD=$2
 
 cd /tmp/
-##install oracle java
+echo "test" >> "deploy.log"
+echo $@ >> "test.log"
+#install oracle java
 sudo -u $ADMIN_USERNAME -p $ADMIN_PASSWORD yum -y install wget
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jdk-8u74-linux-x64.rpm
 sudo -u $ADMIN_USERNAME -p $ADMIN_PASSWORD yum -y install jdk-8u74-linux-x64.rpm
 echo 'install artifactory from bintray'
-##install artifactory from bintray
+#install artifactory from bintray
 wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifactory-pro-rpms.repo
 mv  bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d/
 sudo -u $ADMIN_USERNAME -p $ADMIN_PASSWORD yum -y install artifactory
