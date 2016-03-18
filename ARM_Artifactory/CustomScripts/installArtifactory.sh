25a628d6a5f0f4cccae434fa5f7f75652e9279a6
jroquelaure <jonathanr@jfrog.com>
2016-03-18 16:20:35 +0100

    add auto yes to yum install

--- ARM_Artifactory/CustomScripts/installArtifactory.sh
+++ ARM_Artifactory/CustomScripts/installArtifactory.sh
@@ -1,8 +1,10 @@
 cd /tmp/
+echo 'install jdk 8'
 ##install oracle java
 yum install wget
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jdk-8u74-linux-x64.rpm
 yum -y install jdk-8u74-linux-x64.rpm
+echo 'install artifactory from bintray'
 ##install artifactory from bintray
 wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifactory-pro-rpms.repo
 mv  bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d/
