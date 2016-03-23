adcee32eee2816c90b65fb88901b93de59117c49
jroquelaure <jonathanr@jfrog.com>
2016-03-23 16:58:20 +0100

    fix some install scripts

--- ARM_Artifactory/CustomScripts/installArtifactory.sh
+++ ARM_Artifactory/CustomScripts/installArtifactory.sh
@@ -1,4 +1,5 @@
 export ADMIN_USERNAME=$1
+export CONNECTION_STRING=$2
 
 whoami >> /tmp/azuredeploy.log.$$ 2>&1
 
@@ -15,3 +16,22 @@ wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifac
 mv  bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d/
  yum -y install artifactory
 #service artifactory start
+
+#change the default storage to use installed mysql ressource
+cd /tmp/
+echo  $CONNECTION_STRING >> "connection.log"
+
+#write the new storage.properties file
+printf 'type=mysql\n' >> storage.properties
+printf 'driver=com.mysql.jdbc.Driver\n' >> storage.properties
+printf '%s\n' $CONNECTION_STRING >> storage.properties
+printf 'username=artifactory\n' >> storage.properties
+printf 'password=password\n' >> storage.properties
+
+#copy the storage.properties file to artifactory directory
+cp storage.properties /var/opt/jfrog/artifactory/etc/storage.properties
+
+#get the jdbc driver
+cd /opt/jfrog/artifactory/tomcat/lib
+wget -nv --timeout=30 http://repo.jfrog.org/artifactory/remote-repos/mysql/mysql-connector-java/5.1.24/mysql-connector-java-5.1.24.jar  
+service artifactory start
